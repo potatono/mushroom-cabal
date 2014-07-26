73eb6886e786ee4a0db17112a48001fa3bbc5da7
Justin Day <justin.day@gmail.com>
2014-07-25 22:09:24 -0400

    Authentication

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app = angular.module('chat', ['firebase']);
 
 app._init = function() {
-	app._startTime = (new Date()).getTime();	
+	app._startTime = (new Date()).getTime();
+	app._ref = new Firebase("https://mushroom-cabal.firebaseio.com");
+	app._initAuth();
 
 	// Hacky way to make the room actually change when hash changes.. TODO FIXME
 	$(window).on('hashchange', function() { window.location.reload(); });
@@ -12,6 +14,26 @@ app._init = function() {
 		top.location.href = "http://linkyloo.com/";
 
 	return app;
+}
+
+app._initAuth = function() {
+	if (!app._auth) {
+		app._user = null;
+		app._profile = null;
+		app._auth = new FirebaseSimpleLogin(app._ref, function(error, user) {
+			if (error) {
+   	 			// an error occurred while attempting login
+   	 			console.log(error);
+				$('#loginError').text(error.message.replace("FirebaseSimpleLogin: ","")).removeClass("hide");
+			} 
+			else if (user) {
+				app._setUser(user);
+   	 			console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+			} 
+		});
+	}
+
+	return app._auth;
 }
 
 app._getRoom = function() {
@@ -31,13 +53,14 @@ app._scrollToBottom = function() {
 }
 
 /* Controller for chat */
-app.controller('Chat', ['$scope', '$firebase', 
-    function($scope, $firebase) {
+app.controller('Chat', ['$scope', '$firebase', '$firebaseSimpleLogin',
+    function($scope, $firebase, $firebaseSimpleLogin) {
     	var room = app._getRoom();
-	    var ref = new Firebase('https://mushroom-cabal.firebaseio.com/'+room);
+    	var ref = new Firebase('https://mushroom-cabal.firebaseio.com/rooms/'+room);
 	    // TODO FIXME ref.limit doens't seem to work.
 	    $scope.messages = $firebase(ref.limit(150));
-	    $scope.username = 'Guest' + Math.floor(Math.random()*101);
+	    $scope.user = app._user;
+	    $scope.username = ('Guest' + Math.floor(Math.random()*101))
 
 	    // Adds a message.  If we see a URL we broadcast a URL event so the other
 	    // controller can pick it up.
@@ -76,7 +99,7 @@ app._getTypeFromUrl = function(url) {
 app.controller('Content', ['$scope', '$firebase', '$sce',
 	function($scope, $firebase, $sce) {
 		var room = app._getRoom();
-		var ref = new Firebase('https://mushroom-cabal.firebaseio.com/'+room+'_content');
+		var ref = new Firebase('https://mushroom-cabal.firebaseio.com/content/'+room);
 		$scope.items = $firebase(ref.limit(10));
 		$scope.maxZ = 100;
 
@@ -131,7 +154,7 @@ app.directive('widget', function($sce) {
 app._setupWidget = function(scope, elem, $sce) {
 	scope.$parent.maxZ = Math.max(scope.$parent.maxZ, scope.item.z);
 
-	elem.append('<span class="close">x</span>')
+	elem.append('<span class="close">&times;</span>')
 	elem.append('<h3>'+(scope.item.name || scope.item.type)+'</h3>');
 
 	switch (scope.item.type) {
@@ -212,19 +235,29 @@ app._setupWidgetYoutube = function(scope, elem, $sce) {
 
 				scope.$watch("item.position", function() {
 					player.seekTo(scope.item.position, true);
-				})
+				});
+
+				if (scope.item.position > 0) {
+					player.seekTo(scope.item.position, true);
+				}
+
+				if (scope.item.active) {
+					player.playVideo();
+				}
 			}
 		}
 	});
 
 	var lastTime = 0;
 	var timer = window.setInterval(function() { 
-		var time = player.getCurrentTime();
-		if (Math.abs(time-lastTime) > 5) {
-			scope.item.position = time;
-			scope.$parent.items.$save(scope.id);
+		if (player.getCurrentTime) {
+			var time = player.getCurrentTime();
+			if (Math.abs(time-lastTime) > 5) {
+				scope.item.position = time;
+				scope.$parent.items.$save(scope.id);
+			}
+			lastTime = time;
 		}
-		lastTime = time;
 	}, 250);
 
 
@@ -285,6 +318,119 @@ app._setupWidgetEvents = function(scope,elem) {
 			scope.$parent.items.$save(scope.id);
 		}
 	);
+}
+
+app.signin = function() {
+	$('#loginError').addClass('hide');
+
+	if (!$('#password').val()) 
+		return $('#loginError').text("You didn't supply a password").removeClass('hide') 
+
+	app._auth.login('password', {
+		"email": $('#email').val(),
+		"password": $('#password').val()
+	});
+}
+
+app.signup = function() {
+	$('#loginError').addClass('hide');
+
+	if (!$('#password').val()) 
+		return $('#loginError').text("You didn't supply a password").removeClass('hide') 
+
+	app._auth.createUser($('#email').val(), $('#password').val(), function(error,user) {
+		if (error) {
+			console.log(error);
+			$('#loginError').text(error.message).removeClass("hide");
+		}
+		else {
+			app.signin();
+		}
+	})
+}
+
+app._setUser = function(user) {
+	app._user = user;
+
+   	var $scope = angular.element("#chat").scope();
+   	$scope.$apply(function() { $scope.user = user; });
+   	$('#loginDialog').modal('hide');
+
+   	app._ref.child("@profiles").child(user.uid).on("value",
+   		function(row) {
+   			var profile = row.val();
+
+   			if (profile) {
+   				app._profile = profile;
+   				$scope.$apply(function() { $scope.username = profile.username });
+   			}
+   			else {
+   				$('#profileDialog').modal('show');
+   			}
+   		},
+   		function(error) {
+   			console.log(error);
+   		}
+   	);
+}
+
+app.updateProfile = function() {
+	$('#username').val(app._profile.username);
+	$('#profileError').addClass("hide");
+	$('#profileDialog').modal('show');
+
+	$('#changePassword').toggle(app._user.provider === "password");
+}
+
+app.saveProfile = function() {
+	$('#profileError').addClass("hide");
+	var username = $('#username').val();
+
+	if (username != app._profile.username) {
+		app._ref.child("@usernames").child(username).set({
+			"username": username
+		},
+		function(error) {
+			if (error) {
+				$('#profileError').text("That username is taken").removeClass("hide");
+			}
+			else {
+				app._ref.child("@usernames").child(app._profile.username).remove();
+
+				app._ref.child("@profiles").child(app._user.uid).set({
+					"username": username
+				},
+				function(error) {
+					if (error) {
+						$('#profileError').text(error).removeClass("hide");
+					}
+					else {
+						var $scope = angular.element("#chat").scope();
+						$scope.$apply(function() { $scope.username = username });
+						$('#profileDialog').modal('hide');
+					}
+				});
+			}
+		});
+	}
+
+	var curpass = $('#curpass').val();
+	var newpass = $('#newpass').val();
+	if (curpass && newpass) {
+		app._auth.changePassword(app._user.email, curpass, newpass, function(error, success) {
+			if (error) 
+				$('#profileError').text(error).removeClass("hide");
+			else
+				$('#profileDialog').modal('hide');
+		});
+	}
+}
+
+app.signout = function() {
+	console.log("Logging out");
+	app._auth.logout();
+	// TODO FIXME HACK
+	window.setTimeout(function() { window.location.reload() }, 250);
 }
 
 app._init();
